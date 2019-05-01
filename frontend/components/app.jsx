6689a27934ba5ecffb8ed90495fb7de31711b629
Zalkifl <zcksyed3@gmail.com>
2019-05-01 17:58:20 -0400

    add frontend

---
+++ frontend/components/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import Modal from '../components/modal/modal';
+
+
+const App = () => (
+    <div>    
+        <Modal />
+
+        
+    </div>
+);
+
+export default App; 
