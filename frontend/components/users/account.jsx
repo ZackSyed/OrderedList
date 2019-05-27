e02bd3898cb5901a4af4859be0409a579eda9443
Zalkifl <zcksyed3@gmail.com>
2019-05-27 18:27:15 -0400

    basic account page layout

--- frontend/components/users/account.jsx
+++ frontend/components/users/account.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import deepOrange from '@material-ui/core/colors/deepOrange';
+
+
+const styles = theme => ({
+    root: {
+        margin: 30,
+        backgroundColor: theme.palette.primary.light,
+        padding: 20,
+    },
+    avatar: {
+        margin: 10,
+    },
+    orangeAvatar: {
+        margin: 10,
+        color: '#fff',
+        backgroundColor: deepOrange[500],
+    },
+});
+
 
 class Account extends React.Component {
 
@@ -9,11 +33,18 @@ class Account extends React.Component {
     }
 
     render() {
-        const { currentUser } = this.props;
+        const { currentUser, classes } = this.props;
         return (
             <>
-                <h1>Username: {currentUser.username}</h1>
-                <h2>Email: {currentUser.email}</h2>
+            <Grid container justify="center" alignItems="center">
+                <Grid item xs={12}>
+                    <Paper className={classes.root}>
+                        <Avatar className={classes.avatar}>{currentUser.username[1]}</Avatar>
+                        <Typography variant='h5'>Username: {currentUser.username}</Typography>
+                        <Typography variant='h5'>Email: {currentUser.email}</Typography>
+                    </Paper>
+                </Grid>
+            </Grid>
             </>
         )
     }
@@ -25,4 +56,4 @@ const msp = (state) => {
     };
 };
 
-export default connect(msp, null)(Account);
+export default connect(msp, null)(withStyles(styles)(Account));
